1316fcbe1e16216c868a3a53b2840dd913c34a6b
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2022-07-02 15:30:09 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
 /*
-* main - prints the standrad error sandwiched around the  standard output
-* Return: returns 1
+* main - Entry point
+* Return: Always 0 (sucess)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
